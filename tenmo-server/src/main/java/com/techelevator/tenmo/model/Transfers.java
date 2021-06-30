12cfe5f72217eb519f770e9cc17cb43d8b7aa8e8
Kaylan Barger <ksbarg1@gmail.com>
2021-06-30 10:59:17 -0400

    Transfers Model

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfers.java
@@ -0,0 +1,65 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Transfers {
+
+    
+//Information needed for Transfers
+    private int transferId;
+    private int transferTypeId;
+    private int transferStatusId;
+    private long accountFrom;
+    private long accountTo;
+    private BigDecimal amount;
+
+
+// Setters & Getters
+    public int getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(int transferId) {
+        this.transferId = transferId;
+    }
+
+    public int getTransferTypeId() {
+        return transferTypeId;
+    }
+
+    public void setTransferTypeId(int transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
+
+    public int getTransferStatusId() {
+        return transferStatusId;
+    }
+
+    public void setTransferStatusId(int transferStatusId) {
+        this.transferStatusId = transferStatusId;
+    }
+
+    public long getAccountFrom() {
+        return accountFrom;
+    }
+
+    public void setAccountFrom(long accountFrom) {
+        this.accountFrom = accountFrom;
+    }
+
+    public long getAccountTo() {
+        return accountTo;
+    }
+
+    public void setAccountTo(long accountTo) {
+        this.accountTo = accountTo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
